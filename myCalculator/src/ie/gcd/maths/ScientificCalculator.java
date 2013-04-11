3bb0faed6540500a3f63944b89f7334953c28d36
cjpod <odriscoll195@hotmail.com>
2013-04-11 13:21:04 +0100

    Branch setup

---
+++ myCalculator/src/ie/gcd/maths/ScientificCalculator.java
@@ -0,0 +1,19 @@
+package ie.gcd.maths;
+
+public class ScientificCalculator {
+
+	public ScientificCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+	public int add(int x, int y){
+		return x-y;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
