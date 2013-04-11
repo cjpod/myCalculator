2455aa4e3b10ab0c291a331df199ddbeb9c23472
cjpod <odriscoll195@hotmail.com>
2013-04-11 13:02:27 +0100

    initial commit

---
+++ myCalculator/src/ie/gcd/maths/Calculator.java
@@ -0,0 +1,17 @@
+package ie.gcd.maths;
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
